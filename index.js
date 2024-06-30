a4a7171e071f886e78e552bf8bd722b497d5ba30
joel_patrick <jayjoelpn@gmail.com>
2024-06-30 22:50:19 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function(a, b) {
+    return a/b;
+} 
+console.log (divide (2000, 100));
+const square = (param) => {
+    return param*param
+}
+console.log (square(3));
+const add = (num1, num2)=>{
+    return num1+num2;
+}
+console.log (add (4, 7));
